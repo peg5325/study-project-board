e5511a8013ee3f69004eb399cbf6ae92911adf2f
Eongyu Park <peg5325@gmail.com>
2023-06-08 15:15:16 +0900

    #2 - Spring Data REST 테스트 제외처리
    이것은 이 프로젝트의 비즈니스 로직으로 구현한 내용이 아니라 data rest 기능이고,
    통합테스트라 무거우며 db 에도 영향을 준다.
    공부 목적으로는 의미가 있기 때문에 삭제하지 않고 제외 처리해서 전체 테스트 중에 실행되지 않게끔 처리

--- src/test/java/com/study/studyprojectboard/controller/DataRestTest.java
+++ src/test/java/com/study/studyprojectboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.study.studyprojectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 public class DataRestTest {
+    // 평범한 방식으로는 컨트롤러 테스트 하듯이 할 수 없음.
+    // WebMVC 로 슬라이스 테스트를 적용하려면 그냥은 안되고 방법을 써야하지만 복잡함.
+    // 그래서 평범하게 Data REST 와 관련된 AutoConfiguration을 불러오게 통합 테스트 형태로 진행함.
+    // @SpringBootTest 를 사용하여 필요한 Bean들을 읽은 환경에서 테스트를 진행함.
+    // 그 결과 테스트가 무거워지고 DB 쪽에 Mocking이 안됨. 그래서 DB까지 접근하는 테스트가 되어버림.
 
     private final MockMvc mvc;
 
