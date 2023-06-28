46e5993500f8f718857e6be26907ce12c5059beb
Eongyu Park <peg5325@gmail.com>
2023-06-28 15:10:54 +0900

    #44 - 게시판, 게시글 뷰의 기능에 대응하는 자바 코드 구현

--- src/main/java/com/study/studyprojectboard/domain/constant/SearchType.java
+++ src/main/java/com/study/studyprojectboard/domain/constant/SearchType.java
@@ -1,4 +1,4 @@
-package com.study.studyprojectboard.domain.type;
+package com.study.studyprojectboard.domain.constant;
 
 import lombok.Getter;
 
@@ -14,4 +14,5 @@ public enum SearchType {
     SearchType(String description) {
         this.description = description;
     }
+
 }
